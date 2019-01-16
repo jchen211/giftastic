9a354bb391e81b5cda6029593f70345abed48ea0
Jessica Chen <jchen@jchen.attlocal.net>
2019-01-15 23:45:14 -0600

    trying to get gifs to pause

--- assets/js/gif.js
+++ assets/js/gif.js
@@ -1,6 +1,66 @@
 $(document).ready(function() {
 
-var gameArr = ['Final Fantasy VII', 'The Legend of Zelda: Breath of the Wild', 'Pokémon: Yellow', 'Chrono Trigger'];
+var gameArr = ['Final Fantasy VII', 'Legend of Zelda', 'Pokémon: Yellow', 'Chrono Trigger', 'Mario Kart'];
+
+// this on click function shows the gifs that are already pre-loaded
+function getGifs () {
+
+$('button').on("click", function() {
+
+    var game = $(this).attr('data-game');
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    game + "&api_key=dc6zaTOxFJmzC&limit=6";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+        .then(function(gg) {
+            var results = gg.data;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    var rating = results[i].rating;
+
+                    var gifDiv = $("<div class='gifGames'>");
+
+                    var p = $('<p>').text('Rating: ' + rating);
+
+                    var gameImg = $('<img>');
+
+                    gameImg.addClass('gif-imgs');
+
+                    gameImg.attr('src', results[i].images.fixed_height.url);
+                    gameImg.attr('data-still', results[i].images.fixed_height_still.url);
+                    gameImg.attr('data-state', 'still');
+                    gameImg.attr('data-animate', results[i].images.fixed_height.url);
+
+                    gifDiv.append(p);
+                    gifDiv.append(gameImg);
+
+                    $('#images').prepend(gifDiv);
+
+               
+                }
+            }
+        });
+        $('.gif-imgs').on("click", function() {
+            var state = $(this).attr("data-game");
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          }); 
+          
+    });
+    }
+
+
+
 
 function generateBtn () {
     $('#gameName').empty();
@@ -15,56 +75,19 @@ function generateBtn () {
 }
 
 $("#add-game").on("click", function(e) {
-    event.preventDefault();
+    e.preventDefault();
 
     var games = $('#game-input').val().trim();
     gameArr.push(games);
 
     generateBtn();
-});
 
+    $('#game-form').reset();
+});
+    
+$(document).on('click', '.game-btn', getGifs);
+ 
 generateBtn();
 
-// this on click function shows the gifs that are already pre-loaded
-function getGifs () {
-
-$('button').on("click", function() {
-
-    var game = $(this).attr('data-game');
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    game + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-
-        .then(function(g) {
-            var results = g.data;
-
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    var rating = results[i].rating;
-
-                    var gifDiv = $("<div class='gifGames'>");
-
-                    var p = $('<p>').text('Rating: ' + rating);
-
-                    var gameImg = $('<img>');
-
-                    gameImg.attr('src', results[i].images.fixed_height.url);
-
-                    gifDiv.append(p);
-                    gifDiv.append(gameImg);
-
-                    $('#images').prepend(gifDiv);
-                }
-            }
-        });
-    });
-    }
-
-$(document).on("click", ".game-btn", getGifs);
-
 });
+
